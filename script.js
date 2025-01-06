c586964219fed45d778499b2c86e3131da1f6fe5
Jaumecmprogramacion <jaimecrespoprogramacion@gmail.com>
2025-01-06 12:39:37 +0100

    agregar tarea

--- script.js
+++ script.js
@@ -5,6 +5,23 @@ const enter = document.querySelector('#enter')
 
 //funcion agregar tarea
 function agregarTarea (tarea) {
+    const elemento =  ` 
+                    <li id="elemento">
+                    
+                    <i class="far fa-circle co" data="hecho" id="0"></i>
+                    <p class="text">${tarea}</p>
+                    <i class="fa fa-trash de" data="eliminadi" id="0"></i>
 
-    
+                    </li>
+                     `
+    lista.insertAdjacentHTML("beforeend",elemento)                 
 }
+enter.addEventListener('click',()=>{
+    const tarea = input.value
+    if(tarea){
+        agregarTarea(tarea)
+    }
+    input.value=''
+
+})
+
